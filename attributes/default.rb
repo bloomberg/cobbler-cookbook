57dd7dccd25fb0840dfffb7b43fb282c19f24eff
John Bellone <john.bellone.jr@gmail.com>
2014-06-03 07:23:29 -0400

    Add default attributes and package installs.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,10 @@
 #
 # Copyright (C) 2013, 2014 Bloomberg Finance L.P.
 #
+
+default['389ds']['user'] = 'admin'
+default['389ds']['password'] = 'password'
+default['389ds']['rootdn']['user'] = 'cn=Directory Manager'
+default['389ds']['rootdn']['password'] = 'password'
+default['389ds']['replication']['user'] = 'cn=Replication Manager'
+default['389ds']['replication']['password'] = 'password'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,8 @@
 #
 # Copyright (C) 2013, 2014 Bloomberg Finance L.P.
 #
+
+include_recipe 'chef-sugar::default'
+
+package '389-ds'
+package 'ldap-utils'
