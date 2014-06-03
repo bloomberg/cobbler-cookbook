f01d22ebbc467cf42f1fce17f2307d2ff0f3cf05
John Bellone <john.bellone.jr@gmail.com>
2014-06-03 07:29:36 -0400

    Wrong cookbook. Too many Emacs windows!

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,10 +4,3 @@
 #
 # Copyright (C) 2013, 2014 Bloomberg Finance L.P.
 #
-
-default['389ds']['user'] = 'admin'
-default['389ds']['password'] = 'password'
-default['389ds']['rootdn']['user'] = 'cn=Directory Manager'
-default['389ds']['rootdn']['password'] = 'password'
-default['389ds']['replication']['user'] = 'cn=Replication Manager'
-default['389ds']['replication']['password'] = 'password'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,8 +4,3 @@
 #
 # Copyright (C) 2013, 2014 Bloomberg Finance L.P.
 #
-
-include_recipe 'chef-sugar::default'
-
-package '389-ds'
-package 'ldap-utils'
