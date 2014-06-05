2b6bcd6050d19ab679fb21a57472862061f8f8cb
John Bellone <john.bellone.jr@gmail.com>
2014-06-04 22:25:31 -0400

    Fix tests to turn green.

--- test/integration/default/serverspec/localhost/cobbler_spec.rb
+++ test/integration/default/serverspec/localhost/cobbler_spec.rb
@@ -2,5 +2,4 @@ require 'spec_helper'
 
 describe package('cobbler') do
   it { should be_installed }
-  it { should be_enabled }
 end
--- test/integration/web/serverspec/localhost/cobbler_spec.rb
+++ test/integration/web/serverspec/localhost/cobbler_spec.rb
@@ -2,5 +2,4 @@ require 'spec_helper'
 
 describe package('cobbler-web') do
   it { should be_installed }
-  it { should be_enabled }
 end
