c5a15a3a3dac8946bbfbf2af5d986e124fd2fa5d
John Bellone <john.bellone.jr@gmail.com>
2014-06-15 19:24:19 -0400

    Add helper function in provider for image type.

--- providers/image.rb
+++ providers/image.rb
@@ -4,6 +4,16 @@
 #
 # Copyright (C) 2014 Bloomberg Finance L.P.
 #
+
+def image_type_from_filename(filename)
+  case Filename.extname(filename)
+  when 'iso'
+    return 'iso'
+  else
+    return 'direct'
+  end
+end
+
 action :add do
   basename = File.basename(new_resource.source)
   filename = File.join(Chef::Config[:file_cache_path], basename)
@@ -18,5 +28,13 @@ action :add do
 end
 
 action :remove do
+  basename = File.basename(new_resource.source)
+  filename = File.join(Chef::Config[:file_cache_path], basename)
+
+  file filename do
+    action :delete
+    only_if { File.exist? filename }
+  end
+
   bash "cobbler image remove --name=#{new_resource.name}"
 end
