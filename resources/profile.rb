6da0da53c6d25d3b2368f39347ea24adfd9841ac
John Bellone <john.bellone.jr@gmail.com>
2014-06-15 19:34:28 -0400

    Add profile LWRP for adding kickstart files.

---
+++ resources/profile.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: cobblerd
+# Resource:: profile
+#
+# Copyright (C) 2014 Bloomberg Finance L.P.
+#
+action :add, :remove
+default_action :add
+
+attribute :name, kind_of: String, name_attribute: true
+attribute :distro, kind_of: String
+attribute :kickstart, kind_of: String
