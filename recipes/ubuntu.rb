0d14b692690e555f4823e37284193f8896129d27
Clay Baenziger <cwb@clayb.net>
2014-09-03 04:40:27 -0400

    Update Ubuntu with Saucy 3.11 HWE

--- recipes/ubuntu.rb
+++ recipes/ubuntu.rb
@@ -11,6 +11,10 @@ profile = 'ubuntu'
 cobbler_image 'ubuntu-12.04-minimal' do
   source 'http://archive.ubuntu.com/ubuntu/dists/precise/main/installer-amd64/current/images/netboot/mini.iso'
   checksum '7df121f07878909646c8f7862065ed7182126b95eadbf5e1abb115449cfba714'
+  kernel 'http://archive.ubuntu.com/ubuntu/dists/precise-updates/main/installer-amd64/current/images/saucy-netboot/ubuntu-installer/amd64/linux'
+  kernel_checksum '4664511047fc3d7d5d13be77a86141be05dca283b50a3a51fd3c0638ab72816d'
+  initrd 'http://archive.ubuntu.com/ubuntu/dists/precise-updates/main/installer-amd64/current/images/saucy-netboot/ubuntu-installer/amd64/initrd.gz'
+  initrd_checksum '7f42b6f65bcf5099f69459d14a9b274fe392a2f25e25aba1f54e695cf4135ae3'
   os_version 'precise'
   os_breed 'ubuntu'
 end
