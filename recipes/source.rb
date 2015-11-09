8f5d0bdbe5b359adea9e3e13b054de3c980bb259
Clay Baenziger <cwb@clayb.net>
2015-11-09 04:01:53 -0500

    Clean-up source build (still fails in dpkg-buildpackage -b -us -uc

--- recipes/source.rb
+++ recipes/source.rb
@@ -5,27 +5,32 @@
 # Copyright (C) 2015 Bloomberg Finance L.P.
 #
 
-if node[:platform_family] == "rhel"
-  target_filename = 'cobbler.rpm'
-else if node[:platform_family] == "debian"
-  target_filename = 'cobbler.deb'
-else
-  raise "Unsupported OS family: #{node[:platform_family]}"
-end
+target_filename = 'cobbler.rpm' if node[:platform_family] == "rhel"
+target_filename = 'cobbler.deb' if node[:platform_family] == "debian"
 
 target_filepath = "#{node[:cobbler][:bin_dir]}/#{target_filename}"
 source_code_location = "#{Chef::Config[:file_cache_path]}/cobbler"
 owner = node[:cobbler][:owner]
 group = node[:cobbler][:group]
 
-%w{pyflakes
+%w{git
+   gcc
+   make
+   pyflakes
    pep8
    python-sphinx
    python-cheetah
    python-yaml
    python-nose
+   python-netaddr
+   python-simplejson
+   createrepo
+   python-urlgrabber
+   po-debconf
    debhelper}.each do |pkg|
-  package pkg
+  package pkg do
+    action :install
+  end
 end
 
 git source_code_location do
@@ -36,32 +41,31 @@ git source_code_location do
   not_if { ::File.exist?(target_filepath) }
 end
 
-if node[:platform_family] == "rhel"
+#if node[:platform_family] == "rhel"
+#  bash 'compile cobbler' do
+#    user owner
+#    group group
+#    code %Q{make rpms &&
+#            cp cobbler-*.x86_64.rpm #{target_filepath}
+#    }
+#    cwd source_code_location
+#    action :nothing
+#  end
+#else if node[:platform_family] == "debian"
   bash 'compile cobbler' do
     user owner
     group group
-    code %Q{make rpms
-            cp cobbler-*.x86_64.rpm #{target_filepath}
-    }
-    cwd source_code_location
-    action :nothing
-  end
-else if node[:platform_family] == "debian"
-  bash 'compile cobbler' do
-    user owner
-    group group
-    code %Q{dpkg-buildpackage -b -us -uc
-            rm ../cobbler_*.changes
+    code %Q{make all &&
+            dpkg-buildpackage -b -us -uc &&
+            rm ../cobbler_*.changes &&
             cp ../cobbler_*_all.deb #{target_filepath}
     }
     cwd source_code_location
     action :nothing
   end
-else
-  raise "Unsupported OS family: #{node[:platform_family]}"
-end
+#end
 
 bash "cleanup" do
    code "rm -rf #{source_code_location}"
-   only_if ::File.exist?(source_code_location)
+   only_if { ::File.exist?(source_code_location) }
 end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
 describe_recipe 'cobblerd::default' do
-  it { expect(chef_run).to include_recipe('apt::default') }
-  it { expect(chef_run).not_to include_recipe('yum-epel::default') }
-  it { expect(chef_run).to install_package('cobbler') }
   it { expect(chef_run).to enable_service('cobbler') }
   it { expect(chef_run).to start_service('cobbler') }
+  it { expect(chef_run).not_to run_bash('cobbler sync') }
 end
