f6d8921a27d4c1d2530a09e7d8973077e1dd5fcd
Clay Baenziger <cwb@clayb.net>
2017-01-16 02:55:41 -0500

    Try to prevent mod_python from being initially loaded by apache2 cookbook

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -1,3 +1,9 @@
+# we can not enable both python and wsgi
+# ensure python is disabled (i.e. override Ubuntu 16.04 default)
+apache_module 'python' do
+  enable false
+end
+
 include_recipe 'apache2'
 include_recipe 'apache2::mod_proxy'
 include_recipe 'apache2::mod_proxy_http'
