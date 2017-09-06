12124df204dbf0960ad5f62963b6a2943e8794cf
Clay Baenziger <cbaenziger@bloomberg.net>
2017-09-05 20:59:30 -0400

    Move to a build and install recipe for cobbler source builds

--- recipes/cobbler_source_build.rb
+++ recipes/cobbler_source_build.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: cobblerd
 # Recipe:: cobbler_source
 #
-# Copyright (C) 2015 Bloomberg Finance L.P.
+# Copyright (C) 2017 Bloomberg Finance L.P.
 #
 
 build_user = node['cobbler']['source']['build_user']
@@ -87,38 +87,7 @@ end
     not_if { ::Dir.glob("#{cobbler_target_filepath}/cobbler_*.deb").length > 0 }
   end
 
-  # Cobbler requires mod_version and mod_wsgi in its apache config. make sure
-  # Apache is installed and modules are configured
-  %w{apache2 libapache2-mod-wsgi}.each do |pkg|
-    package pkg do
-      action :install
-    end
-  end
-
-  bash "a2enmod wsgi" do
-    code "a2enmod wsgi"
-    action :run
-    not_if { ::File.exist?("/etc/apache2/mods-enabled/wsgi.load") }
-    notifies :restart, 'service[apache2]', :immediately
-  end
-
-  service 'apache2' do
-    action :nothing
-  end
-
-  # install cobbler
-  cobbler_verify_cmd = 'dpkg-query -W -f=\'${Status}\' cobbler | grep -q \'^install ok installed$\''
-  bash 'install cobbler' do
-    code "dpkg -i #{cobbler_target_filepath} || apt-get install -yf && #{cobbler_verify_cmd}"
-    not_if cobbler_verify_cmd
-  end
- 
-  node.default['cobbler']['service']['name'] = 'cobblerd'
-#end
-
 bash "cleanup" do
    code "rm -rf #{cobbler_code_location}"
    only_if { ::File.exist?(cobbler_code_location) }
 end
-
-include_recipe 'cobblerd::web'
