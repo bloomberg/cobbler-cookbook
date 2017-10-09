f56b47daaef38848bdfe12a77ab7f4f2f4cda2e4
Justin Spies <justin.spies@buyerquest.com>
2017-10-09 10:24:27 -0400

    Fix ChefSpec matchers after renaming the resources

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,52 +1,56 @@
 if defined?(ChefSpec)
   # Creation of a new repository
   def create_cobbler_distro(distro_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:cobblerd_distro, :create, distro_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cobbler_distro, :create, distro_name)
   end
 
   def delete_cobbler_distro(distro_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:cobblerd_distro, :delete, distro_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cobbler_distro, :delete, distro_name)
+  end
+
+  def download_cobbler_iso(iso_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cobbler_iso, :create, image_name)
   end
 
   def import_cobbler_image(image_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:cobblerd_distro, :create, image_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cobbler_image, :create, image_name)
   end
 
   def delete_cobbler_image(image_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:cobblerd_distro, :delete, image_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cobbler_image, :delete, image_name)
   end
 
   def import_cobbler_profile(profile_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:cobblerd_distro, :create, profile_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cobbler_profile, :create, profile_name)
   end
 
   def delete_cobbler_profile(profile_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:cobblerd_distro, :delete, profile_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cobbler_profile, :delete, profile_name)
   end
 
   def import_cobbler_profile(profile_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:cobblerd_distro, :create, profile_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cobbler_profile, :create, profile_name)
   end
 
   def delete_cobbler_profile(profile_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:cobblerd_distro, :delete, profile_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cobbler_profile, :delete, profile_name)
   end
 
   def create_cobbler_repo(repo_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:cobblerd_repo, :create, repo_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cobbler_repo, :create, repo_name)
   end
 
   # Deleting an existing repository
   def delete_cobbler_repo(repo_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:cobblerd_repo, :delete, repo_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cobbler_repo, :delete, repo_name)
   end
 
   def create_cobbler_system(system_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:cobblerd_repo, :create, system_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cobbler_system, :create, system_name)
   end
 
   # Deleting an existing repository
   def delete_cobbler_system(system_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:cobblerd_repo, :delete, system_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:cobbler_system, :delete, system_name)
   end
 end
--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ maintainer_email 'compute@bloomberg.net'
 license          'Apache-2.0'
 description      'Installs/Configures cobblerd'
 long_description 'Installs/Configures cobblerd'
-version          '0.5.2'
+version          '0.5.3'
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
