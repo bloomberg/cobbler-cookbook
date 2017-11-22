a3c1244e62905d9420f456e84562bbca2cf111b0
Justin Spies <justin.spies@buyerquest.com>
2017-11-22 04:26:14 -0500

    Update password generation commentary

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,7 +10,7 @@ node.default['cobblerd']['ks']['username'] = 'cloud'
 
 node.default['cobblerd']['resource_storage'] = '/var/local/cobbler/images/'
 
-# $ echo 'cobbler' | mkpasswd -S LQTvGQ11AIG0k -s -m sha-512
+# $ echo 'cobbler' | mkpasswd -S cobbler -s -m sha-512
 node.default['cobblerd']['ks']['root_password'] = '$6$LQTvGQ11AIG0k$TOSqMnXrQ9Y.3AP6KwRnMitmRaIeteoDKlxVbJgxXB07bK8HdzthHps8gjbIn0iYbTI1BpOVIUtqks6Ed06E7/'
 node.default['cobblerd']['ks']['user_password'] = '$6$LQTvGQ11AIG0k$TOSqMnXrQ9Y.3AP6KwRnMitmRaIeteoDKlxVbJgxXB07bK8HdzthHps8gjbIn0iYbTI1BpOVIUtqks6Ed06E7/'
 
